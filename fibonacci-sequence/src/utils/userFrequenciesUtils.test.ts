5839f46be7086a480144731ff798ee1f4b63c530
Lex Miros <aleksei.miros@outlook.com>
2023-11-27 07:12:38 +1000

    Sneak in one final unit test

--- fibonacci-sequence/src/utils/userFrequenciesUtils.test.ts
+++ fibonacci-sequence/src/utils/userFrequenciesUtils.test.ts
@@ -1,5 +1,7 @@
 import { updateUserFrequencies, sortUserFrequencies } from './userFrequenciesUtils';
 
+const thousandthDigitOfFibonacciSequence = 43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875n
+
 describe('updateUserFrequencies', () => {
   it('should update the frequency of an existing number', () => {
     const userInputFrequencies = new Map([
@@ -26,6 +28,20 @@ describe('updateUserFrequencies', () => {
     expect(updatedFrequencies.get(1n)).toBe(2);
     expect(updatedFrequencies.get(2n)).toBe(1);
     expect(updatedFrequencies.get(3n)).toBe(1);
+  });
+
+  it('should add a new large number to the frequencies map', () => {
+    const userInputFrequencies = new Map([
+      [1n, 2],
+      [2n, 1],
+    ]);
+
+    let updatedFrequencies = updateUserFrequencies(userInputFrequencies, thousandthDigitOfFibonacciSequence);
+    updatedFrequencies = updateUserFrequencies(updatedFrequencies, thousandthDigitOfFibonacciSequence);
+
+    expect(updatedFrequencies.get(1n)).toBe(2);
+    expect(updatedFrequencies.get(2n)).toBe(1);
+    expect(updatedFrequencies.get(thousandthDigitOfFibonacciSequence)).toBe(2);
   });
 
   it('should handle an empty frequencies map', () => {
